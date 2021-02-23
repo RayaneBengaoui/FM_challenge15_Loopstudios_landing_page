b1e5689a230aaa648a57795cbb8e59875ca331ac
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-02-23 17:13:49 +0100

    Finishing the hamburger navigation on tablet view

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const desktopNav = document.querySelector(".desktop-nav");
 
 hamburgerBtn.addEventListener("click", () => {
   desktopNav.classList.toggle("nav-open");
+  if (desktopNav.classList.contains("nav-open")) {
+    hamburgerBtn.children[0].src = "./images/icon-close.svg";
+  } else {
+    hamburgerBtn.children[0].src = "./images/icon-hamburger.svg";
+  }
 });
