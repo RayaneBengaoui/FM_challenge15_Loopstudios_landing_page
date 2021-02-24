f53f70845336da71425ebd1423bda49f85c1324f
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-02-24 15:45:00 +0100

    Removing y-scroll while on tablet/mobile menu

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const hamburgerBtn = document.querySelector(".mobile-nav");
 const desktopNav = document.querySelector(".desktop-nav");
+const body = document.querySelector("body");
 
 hamburgerBtn.addEventListener("click", () => {
   desktopNav.classList.toggle("nav-open");
+  body.classList.toggle("no-scroll");
   if (desktopNav.classList.contains("nav-open")) {
     hamburgerBtn.children[0].src = "./images/icon-close.svg";
   } else {
